99c1eeed2c87769dca1bd1acd269253c57fb5e03
Malikeh97 <malikehehghaghi@gmail.com>
2026-04-03 23:56:42 -0400

    experiment bash file is updated

--- run_experiments.sh
+++ run_experiments.sh
@@ -7,8 +7,8 @@ set -e
 
 source setup/start_env.sh
 
-submit "prisk_pressure_sensitivity" \
-    "python scripts/run_inference.py --experiment configs/experiments/pressure_sensitivity.yaml --resume"
+# submit "prisk_pressure_sensitivity" \
+#     "python scripts/run_inference.py --experiment configs/experiments/pressure_sensitivity.yaml --resume"
 
 # submit "prisk_attack_sensitivity" \
 #     "python scripts/run_inference.py --experiment configs/experiments/attack_sensitivity.yaml --resume"
@@ -18,3 +18,9 @@ submit "prisk_pressure_sensitivity" \
 
 # submit "prisk_open_vs_closed" \
 #     "python scripts/run_inference.py --experiment configs/experiments/open_vs_closed.yaml --resume"
+
+# submit "prisk_attacker_comparison" \
+#     "python scripts/run_inference.py --experiment configs/experiments/attacker_comparison.yaml --resume"
+
+submit "prisk_training_stage_tulu3" \
+    "python scripts/run_inference.py --experiment configs/experiments/training_stage_tulu3.yaml --resume"
